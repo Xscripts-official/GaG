f70251b993cf4ed710893dcfe13ffb778e10a6de
Xscripts-official <wagkuha6@gmail.com>
2025-08-07 00:29:12 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xscripts-official/EggRefresher/refs/heads/main/EggRefresherLATEST"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xscripts-official/GaG/refs/heads/main/menu"))()
